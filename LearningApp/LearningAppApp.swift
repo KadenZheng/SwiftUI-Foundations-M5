2cd57feb333de2aa377b320eafdb44d32b204eee
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2022-01-01 12:41:10 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by LaptopCartUser on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
