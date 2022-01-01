23c4357abe7476a5e196093cbd2622b39fced8e1
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2022-01-01 12:46:40 -0800

    - rename ContentView - organize files into folders - add ContentModel - set ContentModel as environmentObject

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by LaptopCartUser on 1/1/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+    init() {
+        
+    }
+    
+}
